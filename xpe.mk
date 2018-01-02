dcc46ac04d4e901b7c8dafbb6f1462f9eaecf2d3
Carlos Arriaga <carlosarriagacm@gmail.com>
2018-01-02 08:37:58 +0530

    idol3: Welcome XPe :D

--- xpe.mk
+++ xpe.mk
@@ -16,11 +16,9 @@
 
 $(call inherit-product, device/alcatel/idol3/full_idol3.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common XPerience stuff.
+$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := idol3
-PRODUCT_NAME := lineage_idol3
+PRODUCT_NAME := xpe_idol3
 
-# Unofficial Build ID
-TARGET_UNOFFICIAL_BUILD_ID := CarlosArriaga
